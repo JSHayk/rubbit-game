efca94fa3ddcaf5cd9604b6a137d19e52e909a58
JSHayk <jshaykdeveloper@gmail.com>
2022-12-11 02:45:01 +0400

    removed some.js file

--- some.js
+++
@@ -1,5 +0,0 @@
-/*
-    Sax koxer@stuguma sax normala, petqa amena motik@ gtnem
-    stanum em x & y u kanchaum em distance(arjeq),
-    heto mi tex pahum em vor voroshem vorna avle poqr et koxm gnam
-*/
